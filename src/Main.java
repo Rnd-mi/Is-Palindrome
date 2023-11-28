f661b9ad7cf2ff60db5a91c1f6449bed88a7842a
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-11-28 17:48:06 +0300

    add method that checks if a string is palidrome

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    public boolean isPalindrome(String s) {
+
+        for (int i = 0, k = s.length() - 1; i < k; i++, k--) {
+
+            if (s.charAt(i) != s.charAt(k)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
